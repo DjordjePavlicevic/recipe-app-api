3da27bbb2cd6dd90467f43304c6ebe7a5f8cbfde
DjordjePavlicevic <dj.pavlicevic@gmail.com>
2023-07-16 16:41:08 +0200

    improvements

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,8 +2,9 @@
 Django command to wait for database to be available.
 '''
 from django.core.management.base import BaseCommand
-
-
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
 class Command(BaseCommand):
     '''Django command to wait for db'''
 
