407c03d14fa26c5658bdb1b95ba8250d0fa61e38
DjordjePavlicevic <dj.pavlicevic@gmail.com>
2023-07-17 15:04:43 +0200

    test models improved

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,5 +26,5 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().object.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
