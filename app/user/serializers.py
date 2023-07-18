e27279c548ecc765a62b8a2fe824757d764f0b2a
DjordjePavlicevic <dj.pavlicevic@gmail.com>
2023-07-18 14:25:01 +0200

    user auth api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -10,20 +10,30 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password':{'write_only': True, 'min_length': 5}}
-
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for user auth token"""
     email = serializers.EmailField()
     password = serializers.CharField(
-        style= {'input_type': 'password'},
-        trim_whitespace= False,
+        style={'input_type': 'password'},
+        trim_whitespace=False,
     )
 
     def validate(self, attrs):
@@ -31,9 +41,9 @@ class AuthTokenSerializer(serializers.Serializer):
         email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
-            request = self._context.get('request'),
-            username = email,
-            password = password,
+            request=self._context.get('request'),
+            username=email,
+            password=password,
         )
         if not user:
             msg = _('unable to authenticate with provided creds')
